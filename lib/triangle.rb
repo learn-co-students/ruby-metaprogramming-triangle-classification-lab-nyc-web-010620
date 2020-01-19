30de151e4a7574e96a6eea17c6707baaac335390
Filip Todoroski <ftodoroski@ymail.com>
2020-01-19 13:38:09 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,65 +1,60 @@
 class Triangle
-  attr_reader :a, :b, :c
+  attr_accessor  :a, :b, :c
 
-    def initialize(a, b, c)
-      @a = a
-      @b = b
-      @c = c
-    end
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
 
-    def shape
-      [self.a, self.b, self.c]
-    end
-    
-    def equilateral?(shape)
-      shape.all?(shape[0])
-    end
-
-    def scalene?
-      return true if (self.a != self.b) && (self.b != self.c)
-    end
-
-    def isosceles?
-      if (self.a == self.c)
-        return true
-      elsif (self.b == self.c)
-        return true
-      elsif (self.a == self.b)
-        return true
-      end
+  def triangle_inequality?
+    return true if (
+      (self.a + self.b > self.c) && 
+      (self.b + self.c > self.a) && 
+      (self.c + self.a > self.b)
+      )
 
       false
+  end
+
+  def equilateral?
+    [self.a, self.b, self.c].all?(self.a)
+  end
+
+  def scalene?
+    return true if self.triangle_inequality? && ((self.a != self.b) && (self.b != self.c))
+  end
+
+  def isosceles?
+    if self.triangle_inequality? && (self.a == self.c)
+      return true
+    elsif self.triangle_inequality? && (self.b == self.c)
+      return true
+    elsif self.triangle_inequality? && (self.a == self.b)
+      return true
     end
 
-    def triangle_inequality?(shape)
-      return true if (
-        (shape[0] + shape[1] > shape[2]) && 
-        (shape[1] + shape[2] > shape[0]) && 
-        (shape[2] + shape[0] > shape[1])
-        )
+    false
+  end
 
-        false
+  def kind 
+    raise TriangleError if [self.a, self.b, self.c].all?(0)
+    raise TriangleError if self.a < 0 || self.b < 0 || self.c < 0
+
+    if self.equilateral?
+      :equilateral
+    elsif self.isosceles?
+      :isosceles
+    elsif self.scalene?
+      :scalene
+    else
+      raise TriangleError
     end
+  end
 
-    def kind 
-      raise TriangleError if [self.a, self.b, self.c].all?(0)
-      raise TriangleError if self.a < 0 || self.b < 0 || self.c < 0
-
-      if equilateral?(self.shape)
-        :equilateral
-      elsif triangle_inequality?(self.shape) && self.isosceles?
-        :isosceles
-      elsif triangle_inequality?(self.shape) && self.scalene?
-        :scalene
-      else
-        raise TriangleError
-      end
-
+  class TriangleError < StandardError
+    def message
+      "Illegal"
     end
-    
-    class TriangleError < StandardError
-      def message
-          "Illegal"
-      end
-    end
+  end
 end
